148f1117380eef13afe53bf01992b7c035d647bd
kinnnine <nakaharaml9@gmail.com>
2026-03-12 07:28:04 +0700

    config: Added new 3 plugins and changes to GUI
    - Added nvim-rg, neominimap, mini.diff
    - Set cmdheight to 0 for hiding status/cmdbar
    - Set number to true for showing line number
    - Applies some neovide options for boosting performance
    - Remove separators and enable neo-tree extension in lualine
    - Always reload lualine when reloading config
    
    Signed-off-by: kinnnine <nakaharaml9@gmail.com>

--- init.lua
+++ init.lua
@@ -2,7 +2,8 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 -- Vim APIs Helpers
-local globalVar = vim.g
+local globals = vim.g
+local opts = vim.opt
 local callFunc = vim.call
 local execCmd = vim.cmd
 local userCmd = vim.api.nvim_create_user_command
@@ -10,30 +11,51 @@ local userCmd = vim.api.nvim_create_user_command
 -- Plugins
 callFunc('plug#begin')
 Plug('saghen/blink.cmp', { ['tag'] = 'v1.*' })
+Plug('duane9/nvim-rg')
 Plug('nvim-lualine/lualine.nvim')
 Plug('nvim-neo-tree/neo-tree.nvim')
+Plug('Isrothy/neominimap.nvim')
 Plug('romgrk/barbar.nvim')
 Plug('rebelot/kanagawa.nvim')
 Plug('nvim-tree/nvim-web-devicons')
 Plug('MunifTanjim/nui.nvim')
 Plug('nvim-lua/plenary.nvim')
 Plug('rafamadriz/friendly-snippets')
+Plug('nvim-mini/mini.diff')
 callFunc('plug#end')
 
 -- Alias Commands
 userCmd('ReloadConfig', 'source $MYVIMRC', {})
 userCmd('Q', 'quitall', {})
 
+-- Neovim Options
+opts.cmdheight = 0
+opts.number = true
+
 -- Neovide-related Configuration
-if globalVar.neovide then
-	globalVar.neovide_opacity = 0.8
-	globalVar.neovide_normal_opacity = 1
-	globalVar.neovide_refresh_rate = 60
-	globalVar.neovide_theme = 'dark'
+if globals.neovide then
+	globals.neovide_opacity = 0.8
+	globals.neovide_normal_opacity = 1
+	globals.neovide_refresh_rate = 60
+	globals.neovide_cursor_trail_size = 1.0
+	globals.neovide_cursor_animation_length = 0.050
+	globals.neovide_scroll_animation_length = 0.2
+	globals.neovide_floating_blur_amount_x = 0.3
+	globals.neovide_floating_blur_amount_y = 0.3
+	globals.neovide_theme = 'dark'
 end
 
--- Load Plugins
-require('lualine').setup()
+-- Plugins Configuration
+require('lualine').setup({
+	options = {
+		globalstatus = true,
+		component_separators = { left = '', right = ''},
+		section_separators = { left = '', right = ''},
+	},
+	extensions = {
+		'neo-tree',
+	}
+})
 require('neo-tree').setup({
 	close_if_last_window = false,
 	source_selector = {
@@ -59,6 +81,13 @@ require('blink.cmp').setup({
     implementation = "prefer_rust_with_warning"
   },
 })
+globals.neominimap = {
+	mini_diff = {
+		enabled = true,
+	}
+}
+require('mini.diff').setup()
+require('lualine').refresh()
 
 -- Apply colorscheme
 execCmd("colorscheme kanagawa")
